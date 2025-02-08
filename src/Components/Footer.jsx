fbcca189f366699e80555c5c6ca10ecfa65f49bd
Your NameNavithaNelluri <nellurinavitha@example.com>
2025-02-08 13:00:36 -0800

    Commited Successful

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+    <a href='https://github.com/NavithaNelluri' title='Github'>&copy; Navitha 2025</a>
+  </footer>
+  )
+}
+
+export default Footer
